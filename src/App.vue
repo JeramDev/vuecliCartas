3ae1a9c7c759272481ee2c28f76e0d91962e2f43
Jesús Ramos <jesusramosdev@gmail.com>
2019-11-22 20:01:02 +0000

    Ejercicio curso FullStack - cartas con vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main">
+      <BaseCard v-for="(v, i) in values" :key="i" :number= "v" :symbol="currentSymbol"></BaseCard>
+      <div class="butSymbol">
+        <button @click="changeSymbol">CAMBIAR PALO</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseCard from './components/BaseCard'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BaseCard
+  },
+  data () {
+    return {
+      values: ['A', '2', '3', '4', '5', '6',
+        '7', '8', '9', '10', 'J', 'Q', 'K'],
+      symbols: ['♠', '♥', '♦', '♣'],
+      currentSymbol: '♠'
+    }
+  },
+  methods: {
+    changeSymbol () {
+      let currentIndex = this.symbols.indexOf(this.currentSymbol)
+      const nextIndex = currentIndex === this.symbols.length - 1 ? 0 : currentIndex += 1
+      this.currentSymbol = this.symbols[nextIndex]
+      return this.currentSymbol
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .butSymbol {
+    width: 230px;
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  button {
+    height: 150px;
+    width: 180px;
+    font-size: 30px;
+    font-family: monospace;
+    cursor: pointer;
+    border-radius: 10%;
+    box-shadow: 0px 0px 5px 5px;
+    background: yellowgreen;
+  }
+
+  button:hover {
+    background: rgb(162, 218, 51);
+  }
+
+  .main {
+    margin: auto;
+    display: flex;
+    width: 600px;
+    flex-wrap: wrap;
+  }
+
+  .cards {
+    border: 5px solid black;
+    margin: auto;
+    width: 400px;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 20px;
+    margin: 5px;
+  }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <div class="number">{{ number }}</div>
+    <div class="symbol">{{ symbol }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard',
+  props: {
+    number: String,
+    symbol: String
+  }
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Monoton&display=swap');
+
+  .card {
+    margin: 5px;
+    width: 100px;
+    height: 150px;
+    background: linear-gradient(red 50%, black 50%);
+    border: 5px solid black;
+    display: grid;
+    grid-template-rows: repeat(2, 50%);
+    justify-content: space-around;
+    align-items: center;
+    box-shadow: 7px 7px 2px 2px;
+    padding: 5px 0px 5px 0px;
+  }
+
+  .number, .symbol {
+    font-size: 50px;
+    color: white;
+    font-family: 'Monoton';
+  }
+
+  .symbol {
+    font-size: 70px;
+  }
+</style>
